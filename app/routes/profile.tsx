b90a483b1e23e4b82a1f66c7f136a154d356093b
Tawanda <ejmunongo@gmail.com>
2022-12-14 03:24:57 +0200

    user profile page updates

--- app/routes/profile.tsx
+++ app/routes/profile.tsx
@@ -2,12 +2,17 @@ import type { LoaderFunction } from "@remix-run/node";
 import { json, redirect } from "@remix-run/node";
 import { useLoaderData } from "@remix-run/react";
 import PlacesLayout from "~/components/PlacesLayout";
+import UserProfile from "~/components/UserProfile";
 import { getUser } from "~/utils/login.server";
+import prisma from "prisma/db.server.ts";
+import type { User } from "@prisma/client";
 
 type Props = {};
 
 type LoaderData = {
   user: Awaited<ReturnType<typeof getUser>>;
+  user: User;
+  errorMessage?: string;
 };
 
 export const loader: LoaderFunction = async ({ request }) => {
@@ -17,8 +22,19 @@ export const loader: LoaderFunction = async ({ request }) => {
     return redirect("/login");
   }
 
+  dbUser = await prisma.user.findUnique({
+  where: {
+     username:  user.username,
+  },
+  });
+
+  if (!dbUser){
+    return "User not found";
+  }
+
   const data: LoaderData = {
-    user,
+    dbUser,
+    user
   };
 
   return json(data);
@@ -26,14 +42,13 @@ export const loader: LoaderFunction = async ({ request }) => {
 
 const Profile = (props: Props) => {
   const data = useLoaderData<LoaderData>() as unknown as LoaderData;
-
+  // console.log(data);
+  
   return (
     <PlacesLayout>
-      <>
-        <div className="mt-1 md:mt-3">
-          <h3 className="text-center">{data.user?.username}'s Profile</h3>
-        </div>
-      </>
+      <div>
+        <UserProfile />
+      </div>
     </PlacesLayout>
   );
 };
